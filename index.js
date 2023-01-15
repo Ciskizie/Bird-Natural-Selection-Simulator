41a40d7d65186925172505c8a7aa204122ca0776
Ciskizie <cissevdw2005@gmail.com>
2023-01-15 19:27:19 +0100

    birdkiller (half)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let birdCount = 1; //finches
-let Generation = 0; 
+let Generation = 0;
 //let MutationRate= 0.1; //IDK YET
 
 //traits (default settings: Short and thick beak(for seeds), Small size, Brown feathers)
@@ -15,135 +15,169 @@ let area = "forest" //["forest","jungle","tundra"]
 let addBirdAmount = 0
 
 //Summon first bird on load
-window.onload = function() {
-  
+window.onload = function () {
+
   document.getElementById("generationNumber").innerHTML = Generation; // generation 0 on bar
 
-    var startbird = document.createElement('div');
-    startbird.classList.add('startbox');
-    startbird.id = "startbird";
-    document.querySelector('.main').appendChild(startbird);
-    document.getElementById('startbird').innerHTML = '<img src="https://i.ibb.co/wW5cDpV/bird11.png" alt="bird11">';
+  var startbird = document.createElement('div');
+  startbird.classList.add('box');
+  startbird.classList.add('gen0');
+  startbird.id = "startbird";
+  document.querySelector('.main').appendChild(startbird);
+  document.getElementById('startbird').innerHTML = '<img src="https://i.ibb.co/wW5cDpV/bird11.png" alt="bird11">';
 
 
-  var startboxes = document.querySelectorAll('.startbox');
-  startboxes.forEach(function(startbox) {
+  var startboxes = document.querySelectorAll('.box');
+  startboxes.forEach(function (box) {
     let x = Math.random() * 86;
     x = Math.floor(x);
 
     let y = Math.random() * 74;
     y = Math.floor(y);
 
-    console.log(x,y)
-    startbox.style.left = x + "%";
-    startbox.style.top = y + "%"
+    //console.log(x, y)
+    box.style.left = x + "%";
+    box.style.top = y + "%"
   });
 };
 
-function startSimulation(){
-  birdCount +=1
-  console.log("amount of birds is",birdCount);
+function startSimulation() {
+  //birdCount = 2
+  console.log("amount of birds is", birdCount);
   // Add Second Bird
 
   for (var i = 0; i < 1; i++) {
     var bird = document.createElement('div');
     bird.classList.add('box');
+    bird.classList.add('gen0');
     bird.id = "bird";
-    
+
     document.querySelector('.main').appendChild(bird);
 
-   }
-    var boxes = document.querySelectorAll('.box');
-    boxes.forEach(function(box) {
-      box.innerHTML = '<img src="https://i.ibb.co/wW5cDpV/bird11.png" alt="bird11">';
+  }
+  var boxes = document.querySelectorAll('.box');
+  boxes.forEach(function (box) {
+    box.innerHTML = '<img src="https://i.ibb.co/wW5cDpV/bird11.png" alt="bird11">';
 
-      let x = Math.random() * 86;
-      x = Math.floor(x);
-  
-      let y = Math.random() * 74;
-      y = Math.floor(y);
+    let x = Math.random() * 86;
+    x = Math.floor(x);
 
-    console.log(x,y)
+    let y = Math.random() * 74;
+    y = Math.floor(y);
+
+    //console.log(x, y)
     box.style.left = x + "%";
     box.style.top = y + "%"
-    
-  });
-    // Make the button disappear
-    document.getElementById("startbtn").style.display = 'none';
-   
 
-    //move the Generation Bar
-    
-    var i = 0;
-    let Generation = 0;
-    
-    function progressBar() {
-      if (i == 0 ) {
-        i = 1;
-        var elem = document.getElementById("myBar");
-        var width = 1;
-        id = setInterval(frame, 50);//speed
-        running = true;
-        function frame() {
-          if (width >= 100) {
-            clearInterval(id);
-            i = 0;
-           
-            elem.style.width = "0%";
-            
-            if(addBirdAmount < 972){
-              Generation += 1 // generation + 1
-              console.log("generation is",Generation)
-              
-              document.getElementById("generationNumber").innerHTML = Generation;
-              birdReproduction();
-              progressBar(); // call the function again to loop
+  });
+  // Make the button disappear
+  document.getElementById("startbtn").style.display = 'none';
+
+
+  //move the Generation Bar
+
+  var i = 0;
+  let Generation = 0;
+
+  function progressBar() {
+    if (i == 0) {
+      i = 1;
+      var elem = document.getElementById("myBar");
+      var width = 1;
+      id = setInterval(frame, 30);//speed
+      running = true;
+      function frame() {
+        if (width >= 100) {
+          clearInterval(id);
+          i = 0;
+
+          elem.style.width = "0%";
+
+          if (addBirdAmount < 972) {
+            Generation += 1 // generation + 1
+            console.log("generation is", Generation)
+
+            document.getElementById("generationNumber").innerHTML = Generation;
+            birdTerminator();
+
+            birdReproduction();
+
+            progressBar(); // call the function again to loop
           } else {
-              document.getElementById("title").innerHTML = "Birds Dominate the Planet!     (population reached the max limit)"
-            }
-          } else {
-            width++;
-            elem.style.width = width + "%";
-            //console.log(elem.offsetWidth)
-            
+            document.getElementById("title").innerHTML = "Birds Dominate the Planet!     (population reached the max limit)"
           }
-          document.getElementById("pausebtn").addEventListener("click", function(){
-            if(running){
-              clearInterval(id);
-              running = false;
-              this.innerHTML = "Resume"
-            }else{
-              id = setInterval(frame, 50);//speed
-              running = true;
-              this.innerHTML = "Pause"
-            }
-          });
+        } else {
+          width++;
+          elem.style.width = width + "%";
+          //console.log(elem.offsetWidth)
+
         }
+        document.getElementById("pausebtn").addEventListener("click", function () {
+          if (running) {
+            clearInterval(id);
+            running = false;
+            this.innerHTML = "Resume"
+          } else {
+            id = setInterval(frame, 30);//speed
+            running = true;
+            this.innerHTML = "Pause"
+          }
+        });
       }
     }
+  }
 
-    progressBar();
+  progressBar();
 
-  
+
 
 }; // END OF startSimulation
+function birdTerminator() {
+  Generation += 1
 
-function birdReproduction(){
-  Generation += 1 // generation + 1
-  console.log("inside function gen",Generation);
-  let birdGen = Generation; 
+  if (Generation >= 2) {
+    console.log("TERMINATOR!")
+    let birdClass = `${"gen"}${Generation - 2}`
+    console.log(birdClass, "birdclass")
+    document.querySelectorAll("." + birdClass).forEach(e => e.remove()); //!!!!! TERMINATOR
 
-  let newbirdCount = birdCount * 3
-  console.log("amount of birds is",newbirdCount);
+  }
+}
+
+function birdReproduction() {
+
+  console.log("inside function gen", Generation);
+  let boxClassAmount = document.getElementsByClassName("box").length
+
+  console.log("CLASS BOX AMOUNT", boxClassAmount);
+
+  if (boxClassAmount > 250){
+    let = randomFloat = Math.random() * (3.5-2.5)+ 2.5;
+    console.log(randomFloat)
+
+    addBirdAmount = boxClassAmount/randomFloat
+    } else{
+      addBirdAmount = boxClassAmount*3 //FIKSKSKSKKKEN
+    }
+      console.log(addBirdAmount, "=addbirdamount")
+      
+  /*birdCount = boxClassAmount * 3;
+  console.log(birdCount, 'this is birdCount')
+  addBirdAmount = birdCount - boxClassAmount;
+  console.log(addBirdAmount, "=addbirdamount")
+
+  let newbirdCount = birdCount * 3 // birdcoubt is 2
+  console.log("amount of birds is", newbirdCount);
   addBirdAmount = newbirdCount - birdCount;
-  console.log(addBirdAmount,"=addbirdamount")
+  console.log(addBirdAmount, "=addbirdamount")
   birdCount = newbirdCount;
+  */
 
-  let genUpS = "gen"  
-  let genUpN = birdGen; //!!!!!
+  let genUpS = "gen"
+  let genUpN = Generation; //!!!!!
   let genUp = `${genUpS}${genUpN}`;
-  console.log(genUp,"genup");
-  console.log(birdGen,"birdgen")
+  console.log(genUp, "genup");
+
 
   for (var i = 0; i < addBirdAmount; i++) {
     var bird = document.createElement('div');
@@ -151,26 +185,26 @@ function birdReproduction(){
 
     bird.classList.add(genUp)
     bird.id = "bird";
-    
+
     document.querySelector('.main').appendChild(bird);
-   }
-if(Generation == 2){
-   var del = document.getElementsByClassName("box gen1")
-   del.remove;}
-    var boxes = document.querySelectorAll('.box');
-    boxes.forEach(function(box) {
-      box.innerHTML = '<img src="https://i.ibb.co/wW5cDpV/bird11.png" alt="bird11">';
+  }
+ birdCount = document.getElementsByClassName("box").length
+ console.log("amount of birds is", birdCount);
 
-      let x = Math.random() * 86;
-      x = Math.floor(x);
-  
-      let y = Math.random() * 74;
-      y = Math.floor(y);
+  var boxes = document.querySelectorAll('.box');
+  boxes.forEach(function (box) {
+    box.innerHTML = '<img src="https://i.ibb.co/wW5cDpV/bird11.png" alt="bird11">';
 
-    
+    let x = Math.random() * 86;
+    x = Math.floor(x);
+
+    let y = Math.random() * 74;
+    y = Math.floor(y);
+
+
     box.style.left = x + "%";
     box.style.top = y + "%";
-    
+
   });
 
 }
@@ -180,7 +214,7 @@ function enableBeak(n) {
   if (n === 1) {                                  //long and thin
     const btn = document.getElementById('bbtn1');
     btn.style.backgroundColor = "gray";
-    if(availableFood == "insects"){
+    if (availableFood == "insects") {
       console.log("bird can eat insects -> survive");
 
     }
@@ -188,7 +222,7 @@ function enableBeak(n) {
   } else if (n === 2) {                           //short and thick
     const btn = document.getElementById('bbtn2');
     btn.style.backgroundColor = "gray";
-    if(availableFood = "seeds"){
+    if (availableFood = "seeds") {
       console.log("bird can eat seeds -> survive");
 
     }
@@ -196,7 +230,7 @@ function enableBeak(n) {
   } else if (n === 3) {                           //long and curved
     const btn = document.getElementById('bbtn3');
     btn.style.backgroundColor = "gray";
-    if(availableFood == "nectar"){
+    if (availableFood == "nectar") {
       console.log("bird can eat nectar -> survive");
 
     }
@@ -206,8 +240,8 @@ function enableBeak(n) {
     btns.forEach(btn => {
       btn.style.backgroundColor = "";
     });
-    let beakForm = "shortthick"      
-    console.log("birdbeak is reset to ",beakForm);
+    let beakForm = "shortthick"
+    console.log("birdbeak is reset to ", beakForm);
   }
 }
 
@@ -232,7 +266,7 @@ function enableFood(n) {
 }
 
 function enableColor(n) {
-  if (n === 1) {                                  
+  if (n === 1) {
     const btn = document.getElementById('cbtn1');
     btn.style.backgroundColor = "sienna";
   } else if (n === 2) {
@@ -252,7 +286,7 @@ function enableColor(n) {
 }
 
 //environment
-function enableEnv(n) { 
+function enableEnv(n) {
   if (n === 1) {                                  //forest
     const btn = document.getElementById('Forest');
     btn.style.backgroundColor = "gray";
@@ -276,7 +310,7 @@ function enableEnv(n) {
       document.getElementById('simulationBackground').innerHTML = "simulationBackground"
     });
     let size = "small";
-    console.log("birdsize is reset to ",size)
+    console.log("birdsize is reset to ", size)
   }
 }
 
