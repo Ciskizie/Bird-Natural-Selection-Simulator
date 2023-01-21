6c60bbc4a0de404690e38a95eda719e060f271ea
Ciskizie <cissevdw2005@gmail.com>
2023-01-21 15:26:58 +0100

    grafiek almost done

--- index.js
+++ index.js
@@ -39,31 +39,11 @@ window.onload = function () {
     box.style.left = x + "%";
     box.style.top = y + "%"
   });
-  var xValues = [50,60,70,80,90,100,110,120,130,140,150];
-  var yValues = [7,8,8,9,9,9,10,11,14,14,15];
-  
-  new Chart("myChart", {
-    type: "line",
-    data: {
-      labels: xValues,
-      datasets: [{
-        fill: false,
-        lineTension: 0,
-        backgroundColor: "rgba(0,0,255,1.0)",
-        borderColor: "rgba(0,0,255,0.1)",
-        data: yValues
-      }]
-    },
-    options: {
-      legend: {display: false},
-      scales: {
-        yAxes: [{ticks: {min: 6, max:16}}],
-      }
-    }
-  });
+
 };
 
 function startSimulation() {
+
   //birdCount = 2
   console.log("amount of birds is", birdCount);
   // Add Second Bird
@@ -114,14 +94,14 @@ function startSimulation() {
           i = 0;
 
           elem.style.width = "0%";
-
+          addData(chart, [Generation], [birdCount])
           if (addBirdAmount < 972) {
             Generation += 1 // generation + 1
             console.log("generation is", Generation)
 
             document.getElementById("generationNumber").innerHTML = Generation;
             //birdTerminator();
-
+            
             birdReproduction();
 
             progressBar(); // call the function again to loop
